e1c635b86795c4b9bf54283e6a72fda5f8736234
Thales Castro <thalescastrooli98@gmail.com>
2021-12-22 21:29:34 -0300

    docs: add a postman file

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import * as express from 'express';
 import { authenticator } from './middlewares';
 import { UserTable,TransactionTable, AdminTable } from './schemas';
 import { registerAdminRouter, registerRouter, transactionsRouter,balanceRouter } from './routes';
-// import {test} from './test';
 
 const app=express();
 
@@ -16,36 +15,11 @@ app.get('/',(req:any,res:any) =>{
     res.send('Working');
 });
 
-// app.use('/test',test);
-
 app.use('/register',registerRouter);
-
-// app.get('/register/', (req:any,res:any) =>{
-//     if(req.query.cpf && req.query.name && req.query.email){
-//         res.send(users.register(req.query.cpf,req.query.name,req.query.email,0));
-//         // return res.json(users.userTable);
-//     }
-//     else res.send('Insufficient information');
-// })
 
 app.use('/transactions', transactionsRouter);
 
-// app.get('/transactions/', (req:any,res:any) =>{
-//     res.send(transactions.transactions(req.query.user));
-// })
-
 app.use('/balance',balanceRouter);
-
-// app.get('/balance/:id', (req:any,res:any) =>{
-//     res.send(users.userBalance(req.params.id));
-// })
-
-// app.use(express.json());
-
-// app.post('/test2',(req:any,res:any) =>{
-//     console.log(req.body);
-//     res.send(req.body);
-// });
 
 app.use('/register/admin',registerAdminRouter);
 
