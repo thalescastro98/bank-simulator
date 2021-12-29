19b7cdb970e2850272e57decbfa93f1f652033ff
Thales Castro <thalescastrooli98@gmail.com>
2021-12-29 19:30:00 -0300

    feat: add dotenv

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,12 +1,13 @@
 import knex from "knex";
+require('dotenv').config();
 
 export const pg=knex({
     client: 'pg',
     connection: {
-      host : 'localhost',
-      port : 5432,
-      user : 'postgres',
-      password : 'password',
-      database : 'bank_simulator'
+      host : process.env.POSTGRES_HOST || 'localhost',
+      port : parseInt(process.env.POSTGRES_PORT || '5432'),
+      user : process.env.POSTGRES_USER || 'postgres',
+      password : process.env.POSTGRES_PASSWORD,
+      database : process.env.POSTGRES_DATABASE
     }
   });
--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -1,14 +1,15 @@
 // Update with your config settings.
+require('dotenv').config({path:'../../.env'});
 
 module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host : 'localhost',
-      port : 5432,
-      user : 'postgres',
-      password : 'password',
-      database : 'bank_simulator'
+      host : process.env.POSTGRES_HOST || 'localhost',
+      port : parseInt(process.env.POSTGRES_PORT || '5432'),
+      user : process.env.POSTGRES_USER || 'postgres',
+      password : process.env.POSTGRES_PASSWORD,
+      database : process.env.POSTGRES_DATABASE
     }
   }
 };
