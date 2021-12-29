e77435117f57095532d9133ca8d07fb338e93d62
Thales Castro <thalescastrooli98@gmail.com>
2021-12-29 16:19:27 -0300

    style: standardizing the quotes

--- src/database/migrations/20211229040613_create_transactions_table.ts
+++ src/database/migrations/20211229040613_create_transactions_table.ts
@@ -6,7 +6,7 @@ export async function up(knex: Knex): Promise<void> {
     .createTable('transactions',  table => {
         table.uuid('id').primary().notNullable().defaultTo(knex.raw('uuid_generate_v4()')).unique();
         table.string('type',15).notNullable();
-        table.uuid("fromId").notNullable();
+        table.uuid('fromId').notNullable();
         table.decimal('amount',12,2).notNullable();
         table.timestamp('date').defaultTo(knex.fn.now());
         table.string('description',50).notNullable();
