42cf8e0a15c6a04500e6ac13bf82cf26178fe808
Thales Castro <thalescastrooli98@gmail.com>
2021-12-29 22:15:45 -0300

    fix: add new errors

--- src/routes/transactions-router.ts
+++ src/routes/transactions-router.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import { transactionsSchema, getTransactionsSchema, errorHandling, joiHandling } from '../schemas';
+import { transactionsSchema, getTransactionsSchema, errorHandling, joiHandling, ErrorMessage } from '../schemas';
 import { getTransactionsService, transactionsService } from '../service';
 
 export const transactionsRouter = express.Router();
@@ -19,6 +19,8 @@ transactionsRouter.use(express.json());
 transactionsRouter.post('/', async (req: any, res: any) => {
   try {
     const body = joiHandling(transactionsSchema, req.body);
+    if (body.toId && body.type !== 'transfer') throw new ErrorMessage(400, { error: 'toId must be null for this kind of operation.' });
+    if (parseFloat(body.amount) === 0) throw new ErrorMessage(400, { error: 'Transactions with R$0.00 are not allowed' });
     const transaction = await transactionsService(body.type, body.fromId, body.amount, body.toId);
     return res.status(200).send(transaction);
   } catch (err: any) {
