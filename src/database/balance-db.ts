37379b60e33ed99dcca8ecbbb358f8254d1549a4
Thales Castro <thalescastrooli98@gmail.com>
2022-01-02 12:54:16 -0300

    refactor: change get balance logic

--- src/database/balance-db.ts
+++ src/database/balance-db.ts
@@ -3,10 +3,7 @@ import { pg } from '.';
 
 export class BalanceDB {
   static getUserBalance = async (id: string, trx?: Knex.Transaction<any, any[]>) => {
-    let userBalance;
-    if (typeof trx !== 'undefined') userBalance = await trx.raw('select sum(amount) from transactions where "fromId"=?', [id]);
-    else userBalance = await pg.raw('select sum(amount) from transactions where "fromId"=?', [id]);
-    if (userBalance.rows[0].sum === null) return { id: id, balance: '0.00' };
-    return { id: id, balance: userBalance.rows[0].sum as number };
+    const userBalance = await (trx || pg).raw('select COALESCE (sum(amount),0.00) from transactions where "fromId"=?', [id]);
+    return { id: id, balance: userBalance.rows[0].coalesce };
   };
 }
